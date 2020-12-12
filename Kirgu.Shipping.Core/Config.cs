856604b8b42c6eb3b311ee9bbebb462e750d22d2
Gamania <autocardag@gmail.com>
2020-12-12 15:54:16 +0300

    123

--- Kirgu.Shipping.Core/Config.cs
+++ Kirgu.Shipping.Core/Config.cs
@@ -6,9 +6,9 @@ namespace Kirgu.Shipping.API
 {
     public static class Config
     {
-        public const string MongoDB_Address = "";
-        public const string MongoDB_Username = "";
-        public const string MongoDB_Password = "";
+        public const string MongoDB_Address = "mongodb+srv://" + MongoDB_Username + ":" + " + MongoDB_Password + " + "@cluster0.afxq4.mongodb.net/";
+        public const string MongoDB_Username = "MongoKirguUser";
+        public const string MongoDB_Password = "LAKRtLdNXekf8zuS";
         public const string Server_Socket_BindingAddress = "";
         public const int Server_Socket_BindingPort = 0;
         public const string Server_RestApi_BindingAddress = "";
---
+++ Kirgu.Shipping.MongoDBManager/DBManager.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Driver;
+using System;
+using Kirgu.Shipping.API;
+
+namespace Kirgu.Shipping.MongoDBManager
+{
+    public static class DBManager
+    {
+        private MongoClient client = new MongoClient(Config.MongoDB_Address);
+        private IMongoDatabase database = client.GetDatabase("test");
+    }
+}
